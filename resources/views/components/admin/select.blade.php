ad0e5da92abef9527214d7d67d95922e255371e8
Giacomo Piva <me@giacomopiva.com>
2022-05-18 16:27:30 +0200

    Reset branch master

--- resources/views/components/admin/select.blade.php
+++
@@ -1,7 +0,0 @@
-@props(['name', 'options', 'check'])
-
-<select class="form-control" name="{{ $name }}">
-    @foreach ($options as $key => $option)
-        <option value="{{ $key }}" {{ is_selected_option($key, $check ?? '') }}>{{ $option }}</option>
-    @endforeach
-</select>
